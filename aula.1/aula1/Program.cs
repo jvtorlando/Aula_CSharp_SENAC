8e83e543ad48adb27591dd138677a2139820e3f5
jvtorlando <jvorlandoteixiera@gmail.com>
2025-02-26 15:44:25 -0300

    Subindo pasta de aulas C# senac

---
+++ aula.1/aula1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int idade = 19;
+
+            string nomeCompleto = "João";
+
+            var variavelSemTipo = "João";
+
+            Console.WriteLine("Digite o seu nome");
+
+            string nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade");
+
+            string idadeUsuario = Console.ReadLine();
+
+            Console.WriteLine($"Meu nome é {nomeUsuario} e a minha idade é {idadeUsuario}");
+
+
+
+
+        }
+    }
+}
